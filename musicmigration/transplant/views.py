d01feffa41e84c78ae9c6fe7c058df821722e3b4
Jonas Falck <jonas.falck@student.ru.nl>
2018-04-22 01:37:00 +0200

    server

--- musicmigration/transplant/views.py
+++ musicmigration/transplant/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from transplant import getSpotifyCredentials
+from transplant.spotifyfun import getSpotifyCredentials
+
+import spotipy
+import spotipy.util as util
 
 
 def index(request):
     cid, secret, red_url = getSpotifyCredentials()
-    return render(request, 'index.html', {'cid': id, 'secret': secret,
+    return render(request, 'transplant/index.html', {'cid': cid, 'secret': secret,
                                           'red_url': red_url})
+
+def spotify_auth(request):
+    scope = 'user-follow-read'
+    util.prompt_for_user_token()
+    cid, secret, red_url = getSpotifyCredentials()
